7cdad45cf4f8ad6dbdb895741269e0ed8a514430
Rafael <rafaelmelo01111998@gmail.com>
2025-06-11 18:30:15 -0300

    Adicionei uma nova opcao de ATUALIZAR NOTA, alem de agora mostrar a nota dentro do codigo

--- sistema_colegio/sistema.java
+++ sistema_colegio/sistema.java
@@ -19,7 +19,8 @@ public class sistema {
             System.out.println("[2] - LISTAR ALUNOS e QUANTIDADE");
             System.out.println("[3] - BUSCAR ALUNO POR NOME");
             System.out.println("[4] - MEDIA GERAL TURMA");
-            System.out.println("[5] - SAIR DO SISTEMA");
+            System.out.println("[5] - ATUALIZAR NOTA");
+            System.out.println("[6] - SAIR DO SISTEMA");
             System.out.println("Escolha sua opcao: ");
             opcao = input.nextInt();
             input.nextLine();
@@ -38,7 +39,10 @@ public class sistema {
                     System.out.println(alunos.size());
                     System.out.println("TODOS OS ALUNOS \n");
                     for (int i = 0; i <alunos.size(); i++){
-                        System.out.printf("ALUNO [%d]: " + alunos.get(i), i);
+                        System.out.printf("ALUNO [%d]: " + alunos.get(i), i, "\n");
+                        System.out.println(" ");
+                        System.out.printf("NOTA: " + notas.get(i), i, "\n");
+                        System.out.println(" ");
                     }
                     break;
                 case 3:
@@ -54,13 +58,27 @@ public class sistema {
                     System.out.println("MEDIA DOS ALUNOS: " + media);
                     break;
                 case 5:
+                    System.out.println("DIGITE O NOME DO ALUNO PARA ATUALIZAR: ");
+                    String atualizar = input.nextLine();
+                    if (alunos.contains(atualizar)){
+                        System.out.println("Verifique o indice do aluno primeiro antes de botar a NOVA NOTA");
+                        int indice = alunos.indexOf(atualizar);
+                        System.out.println("INDICE: " + indice);
+                        System.out.println("Digite a nova nota do aluno" + atualizar);
+                        double novanota = input.nextDouble();
+                        notas.set(indice, novanota);
+                    } else {
+                        System.out.println("ALUNO INVALIDO.");
+                    }
+                    break;
+                case 6:
                     System.out.println("Saindo do sistema.....");
                     break;
                 default:
                     System.out.println("Opcao invalida");
                     break;
             }
-        } while (opcao != 5);
+        } while (opcao != 6);
         System.out.println("SISTEMA ENCERRADO");
         input.close();
         
