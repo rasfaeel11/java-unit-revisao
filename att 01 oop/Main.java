b9fcb15acc70c4938f3108acf0f13c613d146ba5
Rafael Melo <rafaelmelo01111998@gmail.com>
2025-07-24 15:48:51 -0300

    trouxe de volta os arquivos que tinha excluido.

---
+++ att 01 oop/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Aluno aluno1 = new Aluno();
+
+        // main code
+        System.out.println("Qual o nome do aluno? ");
+        aluno1.setNome(input.nextLine());
+        System.out.println("Quantos foram os acertos desse aluno? ");
+        aluno1.setAcertos(input.nextInt());
+        System.out.println("Quantas questoes foram? ");
+        int questoes = input.nextInt();
+        aluno1.calcularPontuacaoTotal(questoes);
+        aluno1.exibirDados();
+       
+        input.close();
+    }
+}
