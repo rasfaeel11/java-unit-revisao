c27dc73a00d0a03c475b1d5f4a2c6d9552dba816
Rafael <rafaelmelo01111998@gmail.com>
2025-07-24 15:46:37 -0300

    trazendo de volta o att 01

--- att 01 oop/Main.java
+++ att 01 oop/Main.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Aluno aluno1 = new Aluno();
+
+        // main code
         System.out.println("Qual o nome do aluno? ");
         aluno1.setNome(input.nextLine());
         System.out.println("Quantos foram os acertos desse aluno? ");
