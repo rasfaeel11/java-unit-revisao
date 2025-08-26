cbd76319fb6f50b34d62153dd1b46a2301c08061
Rafael Melo <rafaelmelo01111998@gmail.com>
2025-08-26 14:51:53 -0300

    novas atts de abstracao

---
+++ abst_ex_oop/Hotel.java
@@ -0,0 +1,9 @@
+package abst_ex_oop;
+
+public class Hotel {
+    private String nomeHospedePagante;
+    private int duracaoHospedado;
+    private int numerosDeHospede;
+    private int numeroQuartoHospede;
+    private float valorTotalGasto;
+}
---
+++ abst_ex_oop/Streaming.java
@@ -0,0 +1,10 @@
+package abst_ex_oop;
+
+public class Streaming {
+    private String tipoPlano;
+    private String nomeUsuario;
+    private float tempoAssinatura;
+    private int idadeUsuario;
+    private int tipoPagamentoUsuario;
+    private boolean assinaturaCancelada;
+}
